e0cda56bb9c0a8e5b231a30bdc0bf3238bff1855
unknown <docziet@gmail.com>
2025-09-28 12:45:54 +0100

    Task 0 and 1

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-blue-600 text-white p-4 flex justify-between">
+      <h1 className="font-bold">ALX Project</h1>
+      <nav className="space-x-4">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/home">Home Page</Link>
+      </nav>
+    </header>
+  );
+}
