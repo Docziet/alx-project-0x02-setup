a8150e59bf61a6a0cfafd2c24fe3f9153bc4f6d8
unknown <docziet@gmail.com>
2025-09-28 14:36:48 +0100

    Task 6: Integrated Header across pages and added Posts page

--- alx-project-0x02/pages/_app.tsx
+++ alx-project-0x02/pages/_app.tsx
@@ -1,6 +1,14 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components/layout/Header";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <div>
+      <Header />
+      <main className="p-4">
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,3 @@
+export default function PostsPage() {
+  return <h1 className="text-xl font-bold">Posts Page 📄</h1>;
+}
