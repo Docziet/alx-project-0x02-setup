c34cd0ad29fdc297c5871b69b064820326403ad5
unknown <docziet@gmail.com>
2025-09-28 14:13:47 +0100

    Task 3: Created reusable Card component and used in Home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,10 @@
+import Card from "@/components/common/Card";
+
 export default function HomePage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-green-100">
-      <h1 className="text-2xl font-bold text-green-700">This is the Home Page 🏠</h1>
+    <div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Card title="First Card" content="This is the first card." />
+      <Card title="Second Card" content="This is the second card." />
     </div>
   );
 }
